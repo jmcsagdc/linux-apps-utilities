e15f7b2126f01f6df602bb37e785babf3da476f3
jmcsagdc <jasonmakescoolstuff@gmail.com>
2017-12-31 23:45:24 -0800

    Update check_and_sha.py

--- check_and_sha.py
+++ check_and_sha.py
@@ -4,9 +4,17 @@
 so here is a script to analyze the contents of a directory and tell you if it is a file or a dir
 
 also does a sha256sum of each file.
+
+Native python way from python.org that I need to incorporate:
+
+import hashlib, binascii
+dk = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', 100000)
+binascii.hexlify(dk) 
+b'0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5' 
+What I've done below probably won't work...
 """
 
-import os
+import os, hashlib, binascii
 myPath=raw_input('What is the full path you want to check? ')
 os.listdir(myPath)
 
@@ -20,7 +28,9 @@ for each in contentList:
         print(each+' is a directory.\n')
     else:
         print(each+' is a file.')
-        checkTheSum='sha256sum '+each
-        print(checkTheSum)
-        mySum=os.popen(checkTheSum).read()
+        #checkTheSum='sha256sum '+each
+        #print(checkTheSum)
+        #mySum=os.popen(checkTheSum).read()
+        temp = hashlib.pbkdf2_hmac('sha256', each, b'salt', 100000)
+        mySum = binascii.hexlify(temp)
         print(mySum)
