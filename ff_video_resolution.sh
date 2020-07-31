353ff33e0eb65b2899dee0cdb306ec8b12bce358
jmcsagdc <jasonmakescoolstuff@gmail.com>
2020-07-31 14:07:22 -0700

    Update and rename ff_video_resolution to ff_video_resolution.sh

--- ff_video_resolution.sh
+++ ff_video_resolution.sh
@@ -4,7 +4,7 @@ echo $1,$(ffprobe "$1" 2>&1 | grep Stream | grep Video | awk '{print $11}')
 horizontal=$(ffprobe "$1" 2>&1 | grep Stream | grep Video | awk '{print $11}' | awk -Fx '{ print $2 }')
 
 echo "Horizontal resolution: $horizontal"
-if (($horizontal<1000))
+if (($horizontal<800)) #Effective 1080 seems to be 800 as seen in Plex media descriptions of "HD"
 then
   echo SD
 else
