78a48a08a2f8902636148063f3da80f54bb6695e
jmcsagdc <jasonmakescoolstuff@gmail.com>
2021-08-22 10:23:09 -0700

    Update mySimpleWebserver.py

--- mySimpleWebserver.py
+++ mySimpleWebserver.py
@@ -1,5 +1,21 @@
 # Adapted from https://pythonbasics.org/webserver/ with a bunch of my junk thrown in
 
+# To test this, use a separate shell and either run this code block in python directly or as its own script:
+# (you may need to pip install requests if you haven't already...)
+#
+# import requests
+# for i in range (0, 30):
+#     requests.get("http://127.0.0.1:8080").text
+#
+# Output is something like this:
+# 
+# u'Host606'
+# u'Host209'
+# u'Host123'
+# u'Host209'
+# u'Host209'
+# [...]
+
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time, random, os
