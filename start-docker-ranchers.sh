0335d2017d99f3b7520acc882c20210ad22c2137
Jason at Rancher <jasonmakescoolstuff@gmail.com>
2021-11-18 16:22:53 -0800

    fixed duplicate and added note about alias

--- start-docker-ranchers.sh
+++ start-docker-ranchers.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Useful alias: alias dcls='docker container ls | awk "{ print \$1,  \$2 }"'
+
 # Note that the non-linear progression of port numbers skips commonly used ports
 # SEE: https://web.mit.edu/rhel-doc/4/RH-DOCS/rhel-sg-en-4/ch-ports.html
 #
@@ -15,4 +18,4 @@ docker run -d -p 83:80 -p 448:443 --name two-six-head --restart=unless-stopped -
 docker run -d -p 87:80 -p 451:443 --name two-five-seven --restart=unless-stopped --privileged rancher/rancher:v2.5.7
 docker run -d -p 89:80 -p 452:443 --name two-five-ten --restart=unless-stopped --privileged rancher/rancher:v2.5.10
 docker run -d -p 90:80 -p 453:443 --name two-five-eleven --restart=unless-stopped --privileged rancher/rancher:v2.5.11
-docker run -d -p 91:80 -p 454:443 --name two-five-ten --restart=unless-stopped --privileged rancher/rancher:v2.5.10
+docker run -d -p 91:80 -p 454:443 --name two-five-head --restart=unless-stopped --privileged rancher/rancher:v2.5-head
