93b6c5304e33062446e71c60ffa55419dfce1fe3
Jason at Rancher <jasonmakescoolstuff@gmail.com>
2021-11-24 13:14:27 -0800

    Update refresh-rancher-26-head.sh

--- refresh-rancher-26-head.sh
+++ refresh-rancher-26-head.sh
@@ -1,14 +1,14 @@
-echo "\n\nPull new rancher v2.6-head"
-docker pull rancher/rancher:v2.6-head
-
-echo "\n\nRun Rancher 2.6-head on port 455"
-docker run -d -p 92:80 -p 455:443 --name two-six-head --restart=unless-stopped --privileged rancher/rancher:v2.6-head
-
 echo "\n\ndocker container stop two-six-head"
 docker container stop two-six-head
 
 echo  "\n\ndocker container rm two-six-head"
 docker container rm two-six-head
+
+echo "\n\nPull new rancher v2.6-head"
+docker pull rancher/rancher:v2.6-head
+
+echo "\n\nRun Rancher 2.6-head on port 455"
+docker run -d -p 92:80 -p 455:443 --name two-six-head --restart=unless-stopped --privileged rancher/rancher:v2.6-head
 
 echo "\n\nsleep then watch for bootstrap password..."
 sleep 30
