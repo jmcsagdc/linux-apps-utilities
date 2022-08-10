c37d5cdc7bb5a96681b756b59e4ecc3dada3a9a0
Jason Makes Cool Stuff <jasonmakescoolstuff@gmail.com>
2022-08-10 13:31:15 -0700

    Update aliases_and_functions.sh

--- aliases_and_functions.sh
+++ aliases_and_functions.sh
@@ -5,3 +5,4 @@ alias rm="rm -iv"
 alias mv="mv -iv"
 
 dps () { docker ps | awk '{ print $1 "\t\t" $2 "\t\t\t" $4 $5 $6 }' | grep "$1" }
+myhistory () { history | cut -b 8- | sort | uniq | grep -v history | grep $1 }
