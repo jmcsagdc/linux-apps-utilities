fe649c5a29d5a61c46fc4ec1f715c012d2e19a64
Jason Makes Cool Stuff <jasonmakescoolstuff@gmail.com>
2023-02-07 13:22:32 -0800

    Update jpgrenamer.py

--- jpgrenamer.py
+++ jpgrenamer.py
@@ -1,32 +1,44 @@
 #!/usr/bin/env python3
 
-import os
+import os, sys
 
 directory = '.'
 
+print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
 print("I only do jpgs in the current working directory!")
 print("Current working directory: "+os.getcwd())
+print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+myString = input("New filename base: ")
 
-myString = input("new filename base: ")
+print('Suffix is -generative-stock.jpg')
 
 def muddleContents(directory,myString):
     i = 0
     for filename in os.listdir(directory):
-        if ".jpg" in filename:
-            f = os.path.join(directory, filename)
+        f = os.path.join(directory, filename)
+        if ".jpg" in filename:            
             if os.path.isfile(f):
-                print("before: ", f)
+                print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+                print("BEFORE: ", f)
                 i += 1
-                newF = myString+"_"+str(i)+".jpg"
-                print("after: ", newF)
-                doIt = input("Change it y/n? ")
+                newF = myString+"_"+str(i)+"-generative-stock.jpg"
+                print("AFTER: ", newF)
+                print("Use x to EXIT")
+                doIt = input("Change it y/x/n? ")
                 if "y" in doIt:
                     os.rename(filename,newF)
+                elif "x" in doIt:
+                    sys.exit()
                 else:
                     print("I skipped this")
             else:
                 print("Not proper file ", f)
         else:
-            print(filename+" <-----IS NOT A JPEG")
+            if os.path.isdir(f):
+                # print(f+" is a directory")
+                pass
+            else:
+                print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+                print(filename+" <-----IS NOT A JPEG")
 
 muddleContents(directory,myString)
